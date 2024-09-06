18d8deee681f4029406714f27651ff08476e4f01
Bolezy <167684315+Bolezy@users.noreply.github.com>
2024-09-06 20:49:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 const toBool = (x) => x === "true";
 const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "Session~9uHG5zAf",
   HANDLERS:
-    process.env.HANDLER === "false" || process.env.HANDLER === "null"
+    process.env.HANDLER === "false" || process.env.HANDLER === ","
       ? "^"
-      : "[.]",
+      : "[,]",
   BOT_INFO: process.env.BOT_INFO || "ᴀsᴛʀᴏ;ғxᴏᴘ-ᴍᴅ", //YourName;BotName;Image/VideoLink
-  SUDO: process.env.SUDO || "2348039607375,2349162411207,4915252819677",
+  SUDO: process.env.SUDO | ",
   TIME_ZONE: process.env.TIME_ZONE || "Africa/Lagos",
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
@@ -24,7 +24,7 @@ module.exports = {
   HEROKU: toBool(process.env.HEROKU) || false,
   AUTO_READ: toBool(process.env.AUTO_READ) || false,
   RMBG_KEY: process.env.RMBG_KEY || "",
-  AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ) || false,
+  AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ) || true,
   WORK_TYPE: process.env.WORK_TYPE || "private",
   DELETED_LOG: toBool(process.env.DELETED_LOG) || false,
   DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
